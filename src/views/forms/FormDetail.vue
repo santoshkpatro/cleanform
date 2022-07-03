970b9b00a69ba66139dd2fc9815617973f771632
Santosh Kumar Patro <patrosantoshkumar1212@gmail.com>
2022-07-04 01:34:13 +0530

    adding builder featrue

--- src/views/forms/FormDetail.vue
+++ src/views/forms/FormDetail.vue
@@ -20,17 +20,34 @@ onMounted(async () => {
 
 <template>
   <div class="container">
-    <div class="card">
+    <div class="card" v-if="form">
       <div class="card-body">
-        <h5>Name: {{ form.title }}</h5>
-        <button
-          @click="
-            router.push({ name: 'formBuilder', params: { form_id: form.id } })
-          "
-          class="btn btn-sm btn-primary"
-        >
-          Build the form
-        </button>
+        <div class="row">
+          <div class="col-8">
+            <h5>Name: {{ form.title }}</h5>
+            <button
+              @click="
+                router.push({
+                  name: 'formBuilder',
+                  params: { form_id: form.id },
+                })
+              "
+              class="btn btn-sm btn-primary"
+            >
+              Build the form
+            </button>
+          </div>
+          <div class="col-4">
+            <button
+              class="btn btn-sm btn-secondary"
+              @click="
+                router.push({ name: 'formView', params: { slug: form.slug } })
+              "
+            >
+              View link
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   </div>
--- src/views/forms/builder/FormBuilder.vue
+++ src/views/forms/builder/FormBuilder.vue
@@ -9,6 +9,7 @@ const router = useRouter()
 const builder = reactive({
   form: {},
   elements: [],
+  selectedElement: '',
 })
 
 onMounted(async () => {
@@ -28,23 +29,57 @@ onMounted(async () => {
     console.log(e)
   }
 })
+
+function viewSelectedElement(element_id) {
+  builder.selectedElement = builder.elements.find(
+    (element) => element.id === element_id
+  )
+}
 </script>
 
 <template>
-  <div class="builder">
-    <div class="col-2">1</div>
-    <div class="col-2">2</div>
-    <div class="col-2">3</div>
-    <div class="col-2">4</div>
-    <div class="col-2">5</div>
-    <div class="col-2">5</div>
-    <div class="col-2">6</div>
-    <div class="col-2">7</div>
-    <div class="col-2">8</div>
-    <div class="col-2">9</div>
-    <div class="col-2">10</div>
-    <div class="col-2">11</div>
-    <div class="col-2">12</div>
+  <!-- Elements List -->
+  <div class="builder mt-2">
+    <div
+      class="col-2 mx-2"
+      v-for="(element, index) in builder.elements"
+      :key="element"
+      @click="viewSelectedElement(element.id)"
+    >
+      <div class="card">
+        <div class="card-body">
+          <h5 class="text-center">Page - {{ index + 1 }}</h5>
+        </div>
+      </div>
+    </div>
+    <div class="col-2 mx-2">
+      <div class="card">
+        <div class="card-body">
+          <div class="text-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="25"
+              height="25"
+              fill="currentColor"
+              class="bi bi-plus-circle"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
+              />
+              <path
+                d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"
+              />
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Element Editor -->
+  <div class="container mt-5">
+    <div v-if="builder.selectedElement"></div>
   </div>
 </template>
 
