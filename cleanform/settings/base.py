67bfc21ed3bc77298e0a2da4582174e481064919
Santosh Kumar Patro <patrosantoshkumar1212@gmail.com>
2022-07-03 13:46:08 +0530

    DB setup

--- cleanform/settings/base.py
+++ cleanform/settings/base.py
@@ -95,6 +95,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    BASE_DIR / "staticfiles"
+]
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
