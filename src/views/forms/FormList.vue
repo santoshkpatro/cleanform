ae7b356937dc214c52c43e0d06d0c5f2ae8c9c34
Santosh Kumar Patro <patrosantoshkumar1212@gmail.com>
2022-07-04 17:45:45 +0530

    adding new form

--- src/views/forms/FormList.vue
+++ src/views/forms/FormList.vue
@@ -1,10 +1,15 @@
 <script setup>
 import { reactive, onMounted, ref } from 'vue'
 import { useRouter } from 'vue-router'
-import { getForms } from '../../api'
+import { getForms, createForm, updateForm } from '../../api'
 
 const router = useRouter()
 const forms = ref([])
+const newForm = reactive({
+  title: '',
+  description: '',
+  is_live: false,
+})
 
 onMounted(async () => {
   try {
@@ -14,10 +19,50 @@ onMounted(async () => {
     console.log(e)
   }
 })
+
+async function handleUpdateForm(form) {
+  try {
+    const { data } = await updateForm(form.id, form)
+    console.log(data)
+  } catch (e) {
+    console.log(e)
+  } finally {
+    newForm.title = ''
+    newForm.description = ''
+  }
+}
 </script>
 
 <template>
   <div class="container">
+    <!-- Create a new form -->
+    <form @submit.prevent="createNewForm" class="mb-5">
+      <div class="mb-2">
+        <label for="title" class="form-label">Form name</label>
+        <input
+          type="text"
+          class="form-control"
+          v-model="newForm.title"
+          required
+        />
+      </div>
+      <div class="mb-2">
+        <label for="title" class="form-label">Description</label>
+        <textarea
+          class="form-control"
+          id="exampleFormControlTextarea1"
+          rows="3"
+          v-model="newForm.description"
+        ></textarea>
+      </div>
+      <div class="d-grid gap-2">
+        <button type="submit" class="btn btn-sm btn-success">
+          Create form
+        </button>
+      </div>
+    </form>
+
+    <!-- Existing forms -->
     <table class="table">
       <thead>
         <tr>
@@ -25,6 +70,7 @@ onMounted(async () => {
           <th scope="col">Name</th>
           <th scope="col"></th>
           <th scope="col"></th>
+          <th scope="col">Is Live</th>
         </tr>
       </thead>
       <tbody>
@@ -56,6 +102,19 @@ onMounted(async () => {
             >
               View Submissions
             </button>
+          </td>
+          <td>
+            <div class="form-check form-switch">
+              <input
+                v-model="form.is_live"
+                class="form-check-input"
+                type="checkbox"
+                role="switch"
+                id="flexSwitchCheckChecked"
+                :checked="form.is_live"
+                @input="handleUpdateForm({ ...form, is_live: !form.is_live })"
+              />
+            </div>
           </td>
         </tr>
       </tbody>
--- src/views/forms/builder/FormBuilder.vue
+++ src/views/forms/builder/FormBuilder.vue
@@ -1,7 +1,12 @@
 <script setup>
-import { reactive, onMounted } from 'vue'
+import { reactive, onMounted, ref } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
-import { getFormDetails, getFormElements } from '../../../api'
+import {
+  getFormDetails,
+  getFormElements,
+  updateFormElement,
+  createFormElement,
+} from '../../../api'
 
 const route = useRoute()
 const router = useRouter()
@@ -11,6 +16,8 @@ const builder = reactive({
   elements: [],
   selectedElement: '',
 })
+
+const newElement = ref('input_field')
 
 onMounted(async () => {
   const { form_id } = route.params
@@ -35,11 +42,102 @@ function viewSelectedElement(element_id) {
     (element) => element.id === element_id
   )
 }
+
+async function changeElementSetting() {
+  const { form_id } = route.params
+
+  try {
+    const { data } = await updateFormElement(
+      form_id,
+      builder.selectedElement.id,
+      builder.selectedElement
+    )
+    console.log(data)
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+async function addNewElement() {
+  const { form_id } = route.params
+
+  switch (newElement.value) {
+    case 'input_field':
+      try {
+        const { data } = await createFormElement(form_id, {
+          type: 'input_field',
+          label: 'New Input Field',
+          description: '',
+          is_required: false,
+          properties: {
+            type: 'text',
+            placeholder: '',
+          },
+          layouts: {},
+          validations: {},
+        })
+
+        builder.elements.push(data)
+        builder.form.elements.push(data.id)
+      } catch (e) {
+        console.log(e)
+      }
+
+      break
+
+    case 'radio_field':
+      try {
+        const { data } = await createFormElement(form_id, {
+          type: 'radio_field',
+          label: 'New Radio Field',
+          description: '',
+          is_required: false,
+          properties: {
+            options: ['Option 1', 'Option 2', 'Option 3'],
+            default_option: ['Option 2'],
+          },
+          layouts: {},
+          validations: {},
+        })
+
+        builder.elements.push(data)
+        builder.form.elements.push(data.id)
+      } catch (e) {
+        console.log(e)
+      }
+      break
+
+    case 'checkbox_field':
+      try {
+        const { data } = await createFormElement(form_id, {
+          type: 'checkbox_field',
+          label: 'New checkbox Field',
+          description: '',
+          is_required: false,
+          properties: {
+            choices: ['Choice 1', 'Choice 2', 'Choice 3', 'Choice 4'],
+            default_checked_choices: ['Choice 2', 'Choice 3'],
+          },
+          layouts: {},
+          validations: {},
+        })
+
+        builder.elements.push(data)
+        builder.form.elements.push(data.id)
+      } catch (e) {
+        console.log(e)
+      }
+      break
+
+    default:
+      break
+  }
+}
 </script>
 
 <template>
   <!-- Elements List -->
-  <div class="builder mt-2">
+  <div class="container builder mt-2">
     <div
       class="col-2 mx-2"
       v-for="(element, index) in builder.elements"
@@ -47,13 +145,23 @@ function viewSelectedElement(element_id) {
       @click="viewSelectedElement(element.id)"
     >
       <div class="card">
-        <div class="card-body">
+        <div
+          class="card-body"
+          :class="{
+            'bg-dark text-white': builder.selectedElement.id === element.id,
+          }"
+        >
           <h5 class="text-center">Page - {{ index + 1 }}</h5>
+          <p>{{ element.label }}</p>
         </div>
       </div>
     </div>
     <div class="col-2 mx-2">
-      <div class="card">
+      <div
+        class="card"
+        data-bs-toggle="modal"
+        data-bs-target="#addElementModal"
+      >
         <div class="card-body">
           <div class="text-center">
             <svg
@@ -79,7 +187,312 @@ function viewSelectedElement(element_id) {
 
   <!-- Element Editor -->
   <div class="container mt-5">
-    <div v-if="builder.selectedElement"></div>
+    <div v-if="builder.selectedElement">
+      <!-- Input Field Element -->
+      <div class="row" v-if="builder.selectedElement.type === 'input_field'">
+        <div class="col me-2">
+          <label :for="builder.selectedElement.id" class="form-label">{{
+            builder.selectedElement.label
+          }}</label>
+          <input
+            :required="builder.selectedElement.is_required"
+            :id="builder.selectedElement.id"
+            :type="builder.selectedElement.properties.type"
+            class="form-control"
+            :placeholder="builder.selectedElement.properties.placeholder"
+          />
+        </div>
+        <div class="col ms-2">
+          <h5>Settings :</h5>
+          <!-- required setting -->
+          <div class="form-check form-switch mb-2">
+            <input
+              v-model="builder.selectedElement.is_required"
+              class="form-check-input"
+              type="checkbox"
+              role="switch"
+              id="required"
+              :checked="builder.selectedElement.is_required"
+            />
+            <label class="form-check-label" for="required">Required</label>
+          </div>
+
+          <!-- label setting -->
+          <div class="mb-2">
+            <label for="label" class="form-label">Label</label>
+            <input
+              type="text"
+              id="label"
+              class="form-control"
+              v-model="builder.selectedElement.label"
+            />
+          </div>
+
+          <!-- placeholder setting -->
+          <div class="mb-2">
+            <label for="placeholder" class="form-label">Placeholder</label>
+            <input
+              type="text"
+              id="placeholder"
+              class="form-control"
+              v-model="builder.selectedElement.properties.placeholder"
+            />
+          </div>
+
+          <!-- type settting -->
+          <div class="mb-2">
+            <label for="type" class="form-label">Type</label>
+            <select
+              class="form-select"
+              v-model="builder.selectedElement.properties.type"
+            >
+              <option value="text" selected>Text</option>
+              <option value="number">Number</option>
+              <option value="email">Email</option>
+            </select>
+          </div>
+
+          <div class="d-grid gap-2">
+            <button class="btn btn-sm btn-dark" @click="changeElementSetting">
+              Update
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <!-- Radio Field Element -->
+      <div class="row" v-if="builder.selectedElement.type === 'radio_field'">
+        <div class="col me-2">
+          <label :for="builder.selectedElement.id" class="form-label">{{
+            builder.selectedElement.label
+          }}</label>
+
+          <div
+            class="form-check"
+            v-for="option in builder.selectedElement.properties.options"
+            :key="option"
+          >
+            <input
+              class="form-check-input"
+              type="radio"
+              :checked="
+                option === builder.selectedElement.properties.default_option
+              "
+              name="`${builder.selectedElement.label} ${option}`"
+              :id="`${builder.selectedElement.id} ${option}`"
+            />
+            <label
+              class="form-check-label"
+              :for="`${builder.selectedElement.id} ${option}`"
+            >
+              {{ option }}
+            </label>
+          </div>
+        </div>
+        <div class="col ms-2">
+          <h5>Settings:</h5>
+          <!-- required setting -->
+          <div class="form-check form-switch mb-2">
+            <input
+              v-model="builder.selectedElement.is_required"
+              class="form-check-input"
+              type="checkbox"
+              role="switch"
+              id="required"
+              :checked="builder.selectedElement.is_required"
+            />
+            <label class="form-check-label" for="required">Required</label>
+          </div>
+
+          <!-- label setting -->
+          <div class="mb-2">
+            <label for="label" class="form-label">Label</label>
+            <input
+              type="text"
+              id="label"
+              class="form-control"
+              v-model="builder.selectedElement.label"
+            />
+          </div>
+
+          <!-- options setting -->
+          <div class="mb-2">
+            <label for="" class="form-label">Options</label>
+            <input
+              type="text"
+              :value="builder.selectedElement.properties.options.join(',')"
+              @input="
+                (event) =>
+                  (builder.selectedElement.properties.options =
+                    event.target.value.split(','))
+              "
+              class="form-control"
+            />
+          </div>
+
+          <!-- default option setting -->
+          <div class="mb-2">
+            <label for="" class="form-label">Default Option</label>
+            <select
+              class="form-select"
+              v-model="builder.selectedElement.properties.default_option"
+            >
+              <option
+                v-for="option in builder.selectedElement.properties.options"
+                :key="option"
+                :value="option"
+              >
+                {{ option }}
+              </option>
+            </select>
+          </div>
+
+          <div class="d-grid gap-2">
+            <button class="btn btn-sm btn-dark" @click="changeElementSetting">
+              Update
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <!-- Choice Field Element -->
+      <div class="row" v-if="builder.selectedElement.type === 'checkbox_field'">
+        <div class="col me-2">
+          <label :for="builder.selectedElement.id" class="form-label">{{
+            builder.selectedElement.label
+          }}</label>
+          <div
+            class="form-check"
+            v-for="choice in builder.selectedElement.properties.choices"
+            :key="choice"
+          >
+            <input
+              class="form-check-input"
+              type="checkbox"
+              id="flexCheckChecked"
+              :checked="
+                builder.selectedElement.properties.default_checked_choices.includes(
+                  choice
+                )
+              "
+            />
+            <label class="form-check-label" for="flexCheckChecked">
+              {{ choice }}
+            </label>
+          </div>
+        </div>
+        <div class="col ms-2">
+          <h5>Settings:</h5>
+          <!-- required setting -->
+          <div class="form-check form-switch mb-2">
+            <input
+              v-model="builder.selectedElement.is_required"
+              class="form-check-input"
+              type="checkbox"
+              role="switch"
+              id="required"
+              :checked="builder.selectedElement.is_required"
+            />
+            <label class="form-check-label" for="required">Required</label>
+          </div>
+
+          <!-- label setting -->
+          <div class="mb-2">
+            <label for="label" class="form-label">Label</label>
+            <input
+              type="text"
+              id="label"
+              class="form-control"
+              v-model="builder.selectedElement.label"
+            />
+          </div>
+
+          <!-- choice setting -->
+          <div class="mb-2">
+            <label for="" class="form-label">Choices</label>
+            <input
+              type="text"
+              :value="builder.selectedElement.properties.choices.join(',')"
+              @input="
+                (event) =>
+                  (builder.selectedElement.properties.choices =
+                    event.target.value.split(','))
+              "
+              class="form-control"
+            />
+          </div>
+
+          <!-- default checked choices -->
+          <label for="" class="form-label">Default checked choices</label>
+          <div
+            class="form-check"
+            v-for="choice in builder.selectedElement.properties.choices"
+            :key="choice"
+          >
+            <input
+              class="form-check-input"
+              type="checkbox"
+              id="flexCheckChecked"
+              :value="choice"
+              v-model="
+                builder.selectedElement.properties.default_checked_choices
+              "
+            />
+            <label class="form-check-label" for="flexCheckChecked">
+              {{ choice }}
+            </label>
+          </div>
+
+          <div class="d-grid gap-2">
+            <button class="btn btn-sm btn-dark" @click="changeElementSetting">
+              Update
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Add Element Modal -->
+  <div
+    class="modal fade"
+    id="addElementModal"
+    tabindex="-1"
+    aria-labelledby="addElementModal"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="addElementModal">Add Element</h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <form @submit.prevent="addNewElement">
+          <div class="modal-body">
+            <label for="" class="form-label">Select Element type</label>
+            <select class="form-select" v-model="newElement">
+              <option value="input_field" selected>Input Element</option>
+              <option value="radio_field">Radio Element</option>
+              <option value="checkbox_field">Choice Element</option>
+            </select>
+          </div>
+          <div class="modal-footer">
+            <button
+              type="submit"
+              class="btn btn-primary"
+              data-bs-dismiss="modal"
+            >
+              Add Element
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
